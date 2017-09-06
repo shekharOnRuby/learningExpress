a90f0d0c425d94c4e846c84a121c3c82350bca5d
shekhar karande <shekhar.karande@gmail.com>
2017-09-06 22:27:53 +0100

    library tutorial

--- expressFF/express-locallibary-tutorial/routes/users.js
+++ expressFF/express-locallibary-tutorial/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   res.send('respond with a resource');
 });
 
+/* GET users/cool listing. */
+router.get('/cool', function(req, res, next) {
+   res.send("You're so cool");
+});
 module.exports = router;
